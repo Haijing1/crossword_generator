582a3c85b0ccff7e85f49a663cee9ed944ff28b1
Haijing <Haijing.wang@hotmail.com>
2024-10-19 19:47:34 -0700

    add inputs

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,15 @@ function App() {
   return (
     <>
       <h1>Welcome</h1>
+      <h2>Enter crossword puzzle content</h2>
+      <p>See instructions below. Under 50 words is best. </p>
+      <h5>Title</h5>
+      <input type="text" id="inputTitle" name="title"
+        placeholder="Worksheet Title" />
+      <h5>Words</h5>
+      <input type="text" />
+      <h5>Questions</h5>
+      <input type="text" />
     </>
   )
 }
